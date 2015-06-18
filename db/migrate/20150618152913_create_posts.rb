4bfd62295047a7d0e7bcdf22368d9b9d0c50a86e
Taylor Hang <sixthand6th@gmail.com>
2015-06-18 11:43:24 -0400

    Add models and migrations with working associations

---
+++ db/migrate/20150618152913_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title, null:false
+      t.string :text, null:false
+      t.string :url, null:false
+      t.belongs_to :user
+
+      t.timestamps null: false
+    end
+  end
+end
