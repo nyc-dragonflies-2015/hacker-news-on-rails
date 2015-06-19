2de58fee4e517002cc3467164d26151efaa4361b
Taylor Hang <sixthand6th@gmail.com>
2015-06-19 10:09:57 -0400

    add new post route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,30 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        @user = session[:user_id]
+        @user.posts << @post
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+  end
+
   def show
     @post = Post.find_by(id: params[:id])
     @comment = Comment.new()
     @comments = Comment.where(post_id: params[:id])
   end
 
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :url)
+    end
 end
